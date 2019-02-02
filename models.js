a9ae0eeb277313833423ab8b7ce8ec8b51f04e6b
Serena Lertkantitham <lertkantitham@gmail.com>
2019-02-02 13:44:51 -0800

    created schema and virtuals for BlogPost

--- models.js
+++ models.js
@@ -3,5 +3,27 @@
 const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema({
-  
+  title: { type: String},
+  author: {
+    firstName: { type: String },
+    lastName: { type: String }
+  },
+  content: { type: String }
 })
+
+// create virtual so API returns author name as a string rather than an object
+blogPostSchema.virtual("authorName").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`;
+})
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    title: this.title,
+    author: this.authorName,
+    content: this.content
+  }
+}
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
